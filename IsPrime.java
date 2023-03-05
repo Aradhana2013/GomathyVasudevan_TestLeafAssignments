3cc117655b149bde006ee9e62afc1177d3cd1483
Aradhana2013 <126993155+Aradhana2013@users.noreply.github.com>
2023-03-05 17:10:01 -0500

    Add files via upload
    Week1Day1_worked after today's doubt clarifications and new concepts

--- IsPrime.java
+++ IsPrime.java
@@ -5,6 +5,23 @@ public class IsPrime {
 	public static void main(String[] args) {
 		// 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37
 
-		
+		String primenum = "";
+		for (int i = 2; i <= 100; i++) {
+			int num = 0;
+			
+			for (int j = 2; j < i; j++) {
+				if(i%j==0) {
+					num = 1;
+					break;
+				}
+				
+			}
+			if (num==0) {
+				primenum=primenum+i+" ";
+			}
+		}
+System.out.println("Prime numbers upto 100 are given below ");
+System.out.println(primenum);
 	}
+
 }
