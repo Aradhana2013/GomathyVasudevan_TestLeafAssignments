184736a1811cd5458830cc54318bbb46d5891750
Aradhana2013 <126993155+Aradhana2013@users.noreply.github.com>
2023-03-07 11:22:53 -0500

    Add files via upload
    Modified code as per the suggestion and corrected code provided by Vidhya

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,11 +6,12 @@ public class Fibonacci {
 		int firnum = 0;
 		int secnum = 1;
 		int sum = 0;
-
+		System.out.println(firnum);
+		System.out.println(secnum);
 		for (int i = 1; i <= 11; i++) {
 
 			sum = firnum + secnum;
-			System.out.println(firnum);
+			System.out.println(sum);
 			firnum = secnum;
 			secnum = sum;
 		}
